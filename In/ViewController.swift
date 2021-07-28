7c0ad268775c2a422ea0ba42a83a2981f34964fd
Vasu Naidu <vasu@Vasus-Mac-mini.lan>
2021-07-28 10:51:01 +0530

    First commit

---
+++ In/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  In
+//
+//  Created by Vasu Naidu on 27/07/21.
+//  Copyright © 2021 innobox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
+
